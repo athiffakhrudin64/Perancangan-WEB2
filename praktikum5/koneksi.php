d4639bbcf35abf3c520fcc8f048bb65b5bbf6bb8
athiffakhrudin64 <athiffakhrudin64@gmail.com>
2025-12-02 21:18:42 +0700

    Create koneksi.php

---
+++ praktikum5/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+$host = "127.0.0.1"; // jangan localhost
+$user = "root";
+$pass = "";
+$db   = "myweb";
+
+// jika MySQL di port lain, misal 3307:
+$port = 3307; // GANTI sesuai XAMPP kamu
+
+$koneksi = mysqli_connect($host, $user, $pass, $db, $port);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+?>
