800da0c742e9a3493c7c49c36079b7d60edf437b
athiffakhrudin64 <athiffakhrudin64@gmail.com>
2025-12-02 21:10:55 +0700

    Create login.php

---
+++ praktikum6/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login User</title>
+</head>
+<body>
+
+<h2>Form Login</h2>
+
+<form method="post" action="login_process.php">
+    Username : <input type="text" name="username" required><br><br>
+    Password : <input type="password" name="password" required><br><br>
+
+    <input type="submit" value="Login">
+</form>
+
+<br>
+<a href="register.php">Belum punya akun? Daftar</a>
+
+</body>
+</html>
